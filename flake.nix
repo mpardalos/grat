482d80a3270ee49fb5ad9643a4d9299d7859b2ce
Michalis Pardalos <me@mpardalos.com>
2024-03-11 16:14:13 +0000

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "gratgen and gratchk";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        packages.gratgen = pkgs.callPackage ./gratgen.nix
+      }
+    );
+}
+}
